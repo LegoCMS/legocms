886ce4c146a76d9c9cdba45282279237cded7595
Basant Besra <besrabasant@gmail.com>
2022-02-10 12:32:30 +0530

    Add split tree configuration

---
+++ packages/LegoCMS/AdminPanel/src/Providers/AdminPanelServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LegoCMS\AdminPanel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminPanelServiceProvider extends ServiceProvider {
+    public function register()
+    {
+
+    }
+
+    public function boot () {
+
+
+    }
+}
